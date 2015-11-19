ae073bf8c2515c1ad4fd53e08150caf97c2aadb4
Kyle Boss <kyleboss@Kyles-MacBook-Pro.local>
2015-11-19 10:18:25 -0800

    Locations table and Hospital header

--- vendor/bundle/ruby/2.0.0/gems/textacular-3.2.2/spec/support/textacular_web_comic.rb
+++
@@ -1,7 +0,0 @@
-require 'support/ar_stand_in'
-require 'support/character'
-
-class TextacularWebComic < ARStandIn;
-  has_many :characters, :foreign_key => :web_comic_id
-  self.table_name = :web_comics
-end
