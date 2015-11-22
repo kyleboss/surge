3dda0fded49e75ec844ead9e427c888129ad0705
Kyle Boss <kyleboss@airbears2-10-142-67-223.airbears2.1918.berkeley.edu>
2015-11-22 14:25:51 -0800

    Drop departures ONLY if exists

--- db/migrate/20151122024031_create_departures.rb
+++ db/migrate/20151122024031_create_departures.rb
@@ -1,6 +1,6 @@
 class CreateDepartures < ActiveRecord::Migration
   def change
-    drop_table :departures
+    drop_table :departures if (table_exists? :departures)
     create_table :departures do |t|
       t.references :location, index: true, foreign_key: true
       t.references :trackable, index: true, foreign_key: true
